afe0809ed8a7b41b0e7a2922459efe433ff2fe2b
brian <blobo@redhat.com>
2023-03-25 19:08:11 -0300

    Se comienza a agregar swagger

--- src/apps/admin/route.ts
+++ src/apps/admin/route.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { logger } from '../../libs/logs/logs';
 
+const swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
+
 export const router = Router();
 
 router.get('/vars', (req, res) => {
@@ -11,3 +14,26 @@ router.get("/", (req, res) => {
     logger.info(`version: ${process.env.VERSION}`)
     res.json({ version: process.env.VERSION });
 });
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API REST",
+            version: "0.1.0",
+            description:
+                "API REST application made with Express and documented with Swagger",
+        },
+        servers: [
+            {
+                url: "http://localhost:8080",
+            },
+        ],
+    },
+    apis: ['dist/apps/*/route.js'],
+}
+
+const specs = swaggerJsdoc(options)
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }))
+
+
